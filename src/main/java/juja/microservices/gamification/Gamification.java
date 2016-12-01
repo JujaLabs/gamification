d016fc23f80d79aea1feced6555227cb3ae14856
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2016-12-01 22:27:56 +0200

    #5: add test config, fake mongo,nosqlunit-mongodb and clean main config

--- src/main/java/juja/microservices/gamification/Gamification.java
+++ src/main/java/juja/microservices/gamification/Gamification.java
@@ -1,32 +1,12 @@
 package juja.microservices.gamification;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class Gamification {
 
-    @Value("spring.data.mongodb.host")
-    private String host;
-    @Value("spring.data.mongodb.database")
-    private String database;
-
     public static void main(String[] args) {
         SpringApplication.run(Gamification.class, args);
-    }
-
-    @Bean
-    public Mongo mongo() {
-        return new MongoClient(host);
-    }
-
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongo(), database);
     }
 }
---
+++ src/test/java/juja/microservices/gamification/BaseIntegrationTest.java
@@ -0,0 +1,36 @@
+package juja.microservices.gamification;
+
+import com.github.fakemongo.Fongo;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbConfiguration;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import com.lordofthejars.nosqlunit.mongodb.SpringMongoDbRule;
+import com.mongodb.MockMongoClient;
+import com.mongodb.MongoClient;
+import org.junit.Rule;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.inject.Inject;
+
+import static juja.microservices.gamification.GamificationTestConfig.TEST_DATABASE_NAME;
+
+/**
+ * Created by danil.kuznetsov on 01/12/16.
+ */
+@SpringBootTest(classes = {GamificationTestConfig.class, Gamification.class})
+public class BaseIntegrationTest {
+
+    @Rule
+    public MongoDbRule mongoDbRule = getSpringFongoMongoDbRule();
+
+    @Inject
+    protected WebApplicationContext webApplicationContext;
+
+    public static SpringMongoDbRule getSpringFongoMongoDbRule() {
+        MongoDbConfiguration mongoDbConfiguration = new MongoDbConfiguration();
+        mongoDbConfiguration.setDatabaseName(TEST_DATABASE_NAME);
+        MongoClient mongo = MockMongoClient.create(new Fongo("this-fongo-instance-is-ignored"));
+        mongoDbConfiguration.setMongo(mongo);
+        return new SpringMongoDbRule(mongoDbConfiguration);
+    }
+}
