a174fd67abc42150933531f54c6cbad069aafb29
Ivan Tsvelikov <ivan.tsvelikov@gmail.com>
2016-12-09 15:59:10 +0200

    Added tests

---
+++ src/test/java/juja/microservices/gamification/achievement/AchievementDetailUnitTest.java
@@ -0,0 +1,21 @@
+package juja.microservices.gamification.achievement;
+
+import juja.microservices.gamification.achivement.AchievementDetail;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by ВаНо on 09.12.2016.
+ */
+public class AchievementDetailUnitTest {
+    @Test
+    public void idDateExtractorTest(){
+        String sendDateGiven = "583d8e106c51051a60928d62";
+        AchievementDetail detail =
+                new AchievementDetail("userFromId",sendDateGiven,
+                        "description",1);
+        String sendDateFormatted = detail.getSendDate();
+        assertEquals("2016-11-29 16:17:52",sendDateFormatted);
+    }
+}
