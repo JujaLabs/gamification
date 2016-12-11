33d0ef9ad45515e0b070a61dbd7ebb812e2abb53
Garazd <GarazdVZ@gmail.com>
2016-12-11 20:17:21 +0200

    #6: Add new test method size and filling

--- src/test/java/juja/microservices/gamification/achievement/AchievementRepositoryIntegrationTest.java
+++ src/test/java/juja/microservices/gamification/achievement/AchievementRepositoryIntegrationTest.java
@@ -2,9 +2,11 @@ package juja.microservices.gamification.achievement;
 
 import com.lordofthejars.nosqlunit.annotation.ShouldMatchDataSet;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import java.util.List;
 import juja.microservices.gamification.BaseIntegrationTest;
 import juja.microservices.gamification.achivement.Achievement;
 import juja.microservices.gamification.achivement.AchievementRepository;
+import juja.microservices.gamification.achivement.UserPointsSum;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +15,7 @@ import javax.inject.Inject;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by danil.kuznetsov on 01/12/16.
@@ -31,5 +34,26 @@ public class AchievementRepositoryIntegrationTest extends BaseIntegrationTest {
 
         String actualId = achievementRepository.addAchievement(testAchievement);
         assertThat(actualId,notNullValue());
+    }
+
+    @Test
+    @UsingDataSet(locations = "/datasets/addNewUsersAndAchievement.json")
+    public void shouldReturnSizeAllUsersWithAchievement() {
+        List<UserPointsSum> list = achievementRepository.getAllUsersWithAchievement();
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    @UsingDataSet(locations = "/datasets/addNewUsersAndAchievement.json")
+    public void shouldReturnAllUsersNameAndSumAchievement() {
+        List<UserPointsSum> list = achievementRepository.getAllUsersWithAchievement();
+
+        list.get(0).getUserToId().equals("peter");
+        int expectedSumPointUserPeter = 6;
+        assertEquals(expectedSumPointUserPeter, list.get(0).getPointCount());
+
+        list.get(1).getUserToId().equals("max");
+        int expectedSumPointUserMax = 4;
+        assertEquals(expectedSumPointUserMax, list.get(1).getPointCount());
     }
 }
