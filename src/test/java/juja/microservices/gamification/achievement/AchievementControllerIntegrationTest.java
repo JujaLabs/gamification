73cdf71f83d5b642f7ed418a6b4f7db2578975d4
Ivan Tsvelikov <ivan.tsvelikov@gmail.com>
2016-12-30 15:07:01 +0200

    #15 whitespaces deleted =)

--- src/test/java/juja/microservices/gamification/achievement/AchievementControllerIntegrationTest.java
+++ src/test/java/juja/microservices/gamification/achievement/AchievementControllerIntegrationTest.java
@@ -11,9 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.util.regex.Pattern;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
