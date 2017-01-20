0ab36362ebbb22fef5b948b95f7941b46de7cfb2
Ivan Tsvelikov <ivan.tsvelikov@gmail.com>
2017-01-20 12:10:44 +0200

    #15 fixed all mistakes and removed unnecessary methods

--- src/main/java/juja/microservices/gamification/Controller/AchievementController.java
+++ src/main/java/juja/microservices/gamification/Controller/AchievementController.java
@@ -2,9 +2,10 @@ package juja.microservices.gamification.Controller;
 
 import juja.microservices.gamification.DAO.AchievementRepository;
 import juja.microservices.gamification.Entity.Achievement;
-import net.minidev.json.JSONObject;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.inject.Inject;
 
 @RestController
