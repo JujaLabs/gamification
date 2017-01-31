4e20c17076932478510d0a75e22b11f1eaef7d67
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-01-31 23:55:28 +0200

    fix controller tests with dependencies by url

--- src/main/java/juja/microservices/gamification/controller/AchievementController.java
+++ src/main/java/juja/microservices/gamification/controller/AchievementController.java
@@ -1,5 +1,7 @@
 package juja.microservices.gamification.controller;
 
+import juja.microservices.gamification.dao.AchievementRepository;
+import juja.microservices.gamification.entity.Achievement;
 import juja.microservices.gamification.service.AchievementService;
 import net.minidev.json.JSONObject;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +18,24 @@ public class AchievementController {
 
     @Inject
     private AchievementService achievementService;
+    @Inject
+    private AchievementRepository achievementRepository;
 
     @RequestMapping(value = "/achieve", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> createAchievement(@RequestBody Achievement achievement) {
+        String achievementId = achievementRepository.addAchievement(achievement);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", achievementId);
+        return ResponseEntity.ok(jsonObject);
+    }
+
+    @RequestMapping(value = "/achieve/daily", method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity<?> addDaily(@RequestParam String report, String userFromId) {
         String achievementId = achievementService.addDaily(report, userFromId);
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id",achievementId);
+        jsonObject.put("id", achievementId);
         return ResponseEntity.ok(jsonObject);
     }
 }
