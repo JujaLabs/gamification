b9ef4e7f8ff096527927082dd3c22011ad092fe0
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-02-06 22:05:13 +0200

    add acceptace test

---
+++ src/test/java/juja/microservices/acceptance/UserControllerAcceptanceTest.java
@@ -0,0 +1,82 @@
+package juja.microservices.acceptance;
+
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import juja.microservices.gamification.Gamification;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbConfigurationBuilder.mongoDb;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * Created by danil.kuznetsov on 05/02/17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {Gamification.class})
+@DirtiesContext
+public class UserControllerAcceptanceTest {
+
+    @LocalServerPort
+    int localPort;
+
+    @Rule
+    public MongoDbRule mongoDbRule = new MongoDbRule(
+            mongoDb()
+                    .databaseName("gamification")
+                    .host("127.0.0.1")
+                    .port(27017)
+                    .build()
+    );
+
+    @Before
+    public void setup() {
+        RestAssured.port = localPort;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @UsingDataSet(locations = "/datasets/selectAchievementById.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    @Test
+    public void testUrlAchieveDetails() {
+        Response result = given()
+                .contentType("application/json")
+                .when()
+                .get("/user/achieveDetails")
+                .then()
+                .statusCode(200)
+                .assertThat().body(matchesJsonSchemaInClasspath("jsonSchema/responseAchieveDetails.json"))
+                .extract()
+                .response();
+        System.out.println("\n\nResponse for /user/achieveDetails");
+        System.out.println("\n\n"+result.asString()+"\n\n");
+    }
+
+    @UsingDataSet(locations = "/datasets/addNewUsersAndAchievement.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    @Test
+    public void testUrlAchieveSum() {
+        Response result = given()
+                .contentType("application/json")
+                .when()
+                .get("/user/achieveSum")
+                .then()
+                .statusCode(200)
+                .assertThat().body(matchesJsonSchemaInClasspath("jsonSchema/responseAchieveSum.json"))
+                .extract()
+                .response();
+
+        System.out.println("\n\nResponse for /user/achieveSum");
+        System.out.println("\n\n"+result.asString()+"\n\n");
+    }
+}
--- src/test/java/juja/microservices/gamification/GamificationTestConfig.java
+++ src/test/java/juja/microservices/gamification/GamificationTestConfig.java
@@ -2,8 +2,8 @@ package juja.microservices.gamification;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import javax.inject.Inject;
@@ -11,7 +11,7 @@ import javax.inject.Inject;
 /**
  * @author danil.kuznetsov
  */
-@Configuration
+@TestConfiguration
 public class GamificationTestConfig extends AbstractMongoConfiguration {
 
     public static final String TEST_DATABASE_NAME = "gamification-test";
