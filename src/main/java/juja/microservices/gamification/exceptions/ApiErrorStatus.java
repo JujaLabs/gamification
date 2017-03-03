212c4f59722ce40ec24570393027394b165881cf
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-03-03 10:59:42 +0200

    #40: add other exception

--- src/main/java/juja/microservices/gamification/exceptions/ApiErrorStatus.java
+++ src/main/java/juja/microservices/gamification/exceptions/ApiErrorStatus.java
@@ -4,8 +4,11 @@ package juja.microservices.gamification.exceptions;
  * @author Danil Kuznetsov
  */
 public enum ApiErrorStatus {
-    GAMIFICATION_EXCEPTION(0, "Oops something went wrong :(", "The exception is general in gamification"),
-
+    GAMIFICATION_EXCEPTION(
+            0,
+            "Oops something went wrong :(",
+            "The exception is general in gamification"
+    ),
 
     OTHER_EXCEPTION(
             2,
@@ -19,17 +22,47 @@ public enum ApiErrorStatus {
             "The exception is internal Spring exceptions"
     ),
 
-
-    SPRING_NOT_VALID_REQUEST_EXCEPTION (
+    SPRING_NOT_VALID_REQUEST_EXCEPTION(
             101,
             "You request is not valid",
             "The exception is Spring exceptions: BindException or MethodArgumentNotValidException"
     ),
 
-    SPRING_REQUEST_PARAMETER_NOT_FOUND_EXCEPTION (
+    SPRING_REQUEST_PARAMETER_NOT_FOUND_EXCEPTION(
             102,
             "In your request is missing parameters",
             "The exception is Spring exceptions: MissingServletRequestPartException or MissingServletRequestParameterException"
+    ),
+
+    SPRING_TYPE_MISMATCH(
+            103,
+            "Type mismatch",
+            "This exception is thrown when try to set bean property with wrong type. Exception - TypeMismatchException"
+    ),
+    SPRING_HTTP_MEDIA_TYPE_NOT_SUPPORTED(
+            103,
+            "Unsupported media type",
+            "The client send a request with unsupported media type. Exception - HttpMediaTypeNotSupportedException"
+    ),
+    SPRING_HTTP_REQUEST_METHOD_NOT_SUPPORTED(
+            104,
+            "Unsupported HTTP method",
+            "You send a requested with an unsupported HTTP method. Exception - HttpRequestMethodNotSupportedException"
+    ),
+    SPRING_HTTP_MEDIA_TYPE_NOT_ACCEPTABLE(
+            105,
+            "Not acceptable media type",
+            "The client send a request with not acceptable media type. Exception - HttpMediaTypeNotAcceptableException"
+    ),
+    SPRING_PATH_VARIABLE_NOT_FOUND_EXCEPTION(
+            106,
+            "In your request is missing variables",
+            "The exception is Spring exceptions: MissingPathVariableException"
+    ),
+    SPRING_NO_HANDLER_FOUND_EXCEPTION(
+            107,
+            "404 response",
+            "The exception is Spring exceptions: NoHandlerFoundException"
     );
 
     private String developerMessage;
--- src/main/java/juja/microservices/gamification/exceptions/ApiExceptionsHandler.java
+++ src/main/java/juja/microservices/gamification/exceptions/ApiExceptionsHandler.java
@@ -1,15 +1,24 @@
 package juja.microservices.gamification.exceptions;
 
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
@@ -53,22 +62,118 @@ public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
 
 
     private ApiErrorMessage convertToApiErrorMessage(Exception ex, HttpStatus status) {
-        if (ex instanceof MethodArgumentNotValidException) {
-            return messageArgumentNotValid((MethodArgumentNotValidException) ex, status);
+
+        if (ex instanceof HttpRequestMethodNotSupportedException) {
+            return messageHttpRequestMethodNotSupported((HttpRequestMethodNotSupportedException) ex, status);
+        } else if (ex instanceof HttpMediaTypeNotSupportedException) {
+            return messageHttpMediaTypeNotSupported((HttpMediaTypeNotSupportedException) ex, status);
+        } else if (ex instanceof HttpMediaTypeNotAcceptableException) {
+            return messageHttpMediaTypeNotAcceptable((HttpMediaTypeNotAcceptableException) ex, status);
+        } else if (ex instanceof MissingPathVariableException) {
+            return messageMissingPathVariable((MissingPathVariableException) ex, status);
         } else if (ex instanceof MissingServletRequestParameterException) {
             return messageMissingServletRequestParameter((MissingServletRequestParameterException) ex, status);
+        } else if (ex instanceof ConversionNotSupportedException) {
+            return messageConversionNotSupported((ConversionNotSupportedException) ex, status);
+        } else if (ex instanceof TypeMismatchException) {
+            return messageTypeMismatch((TypeMismatchException) ex, status);
+        } else if (ex instanceof MethodArgumentNotValidException) {
+            return messageMethodArgumentNotValid((MethodArgumentNotValidException) ex, status);
+        } else if (ex instanceof MissingServletRequestPartException) {
+            return messageMissingServletRequestPart((MissingServletRequestPartException) ex, status);
+        } else if (ex instanceof BindException) {
+            return messageBindException((BindException) ex, status);
+        } else if (ex instanceof NoHandlerFoundException) {
+            return messageNoHandlerFoundException((NoHandlerFoundException) ex, status);
+        } else {
+            return messageOtherSpringExceptions(ex, status);
         }
-
-        return null;
     }
 
+    private ApiErrorMessage messageNoHandlerFoundException(NoHandlerFoundException ex, HttpStatus httpStatus) {
+        String error = "No handler found for " + ex.getHttpMethod() + " " + ex.getRequestURL();
 
-    /**
-     * Scenarios of a client send an invalid request to API
-     * Handler:
-     * MethodArgumentNotValidException
-     */
-    private ApiErrorMessage messageArgumentNotValid(MethodArgumentNotValidException ex, HttpStatus httpStatus) {
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_NO_HANDLER_FOUND_EXCEPTION)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(error)
+                .build();
+    }
+
+    private ApiErrorMessage messageMissingPathVariable(MissingPathVariableException ex, HttpStatus httpStatus) {
+        String error = ex.getVariableName() + " uri variable is missing";
+
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_PATH_VARIABLE_NOT_FOUND_EXCEPTION)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(error)
+                .build();
+    }
+
+    private ApiErrorMessage messageHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpStatus httpStatus) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(" Media type is not acceptable. Acceptable media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t + ", "));
+
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_HTTP_MEDIA_TYPE_NOT_ACCEPTABLE)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(builder.toString())
+                .build();
+    }
+
+    private ApiErrorMessage messageHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpStatus httpStatus) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t + ", "));
+
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_HTTP_MEDIA_TYPE_NOT_SUPPORTED)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(builder.toString())
+                .build();
+    }
+
+    private ApiErrorMessage messageHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpStatus httpStatus) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getMethod());
+        builder.append(" method is not supported for this request. Supported methods are ");
+        ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
+
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_HTTP_REQUEST_METHOD_NOT_SUPPORTED)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(builder.toString())
+                .build();
+    }
+
+    private ApiErrorMessage messageConversionNotSupported(ConversionNotSupportedException ex, HttpStatus httpStatus) {
+        String error = ex.getPropertyName() + " should be of type " + ex.getRequiredType().getName();
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_TYPE_MISMATCH)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(error)
+                .build();
+    }
+
+    private ApiErrorMessage messageTypeMismatch(TypeMismatchException ex, HttpStatus httpStatus) {
+        String error = ex.getPropertyName() + " should be of type " + ex.getRequiredType().getName();
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_TYPE_MISMATCH)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(error)
+                .build();
+    }
+
+    private ApiErrorMessage messageBindException(BindException ex, HttpStatus httpStatus) {
         List<String> errors = new ArrayList<>();
 
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
@@ -85,23 +190,52 @@ public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
                 .build();
     }
 
-    /**
-     * Scenarios when part of multipart request not found or when request missing parameter
-     * Handler:
-     * MissingServletRequestParameterException
-     */
-    private ApiErrorMessage messageMissingServletRequestParameter(MissingServletRequestParameterException ex,
-                                                                  HttpStatus httpStatus) {
+    private ApiErrorMessage messageMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpStatus httpStatus) {
+        List<String> errors = new ArrayList<>();
 
-        String error = ex.getParameterName() + " parameter is missing";
-
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + " : " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + " : " + error.getDefaultMessage());
+        }
         return ApiErrorMessage
                 .builder(ApiErrorStatus.SPRING_NOT_VALID_REQUEST_EXCEPTION)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailErrors(errors)
+                .build();
+    }
+
+    private ApiErrorMessage messageMissingServletRequestPart(MissingServletRequestPartException ex, HttpStatus httpStatus) {
+        String error = ex.getRequestPartName() + " parameter is missing";
+
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_REQUEST_PARAMETER_NOT_FOUND_EXCEPTION)
                 .httpStatus(httpStatus.value())
                 .exceptionMessage(ex.getMessage())
                 .detailError(error)
                 .build();
     }
 
+    private ApiErrorMessage messageMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                                  HttpStatus httpStatus) {
+        String error = ex.getParameterName() + " parameter is missing";
+
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_REQUEST_PARAMETER_NOT_FOUND_EXCEPTION)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .detailError(error)
+                .build();
+    }
+
+    private ApiErrorMessage messageOtherSpringExceptions(Exception ex, HttpStatus httpStatus) {
+        return ApiErrorMessage
+                .builder(ApiErrorStatus.SPRING_EXCEPTION)
+                .httpStatus(httpStatus.value())
+                .exceptionMessage(ex.getMessage())
+                .build();
+    }
 
 }
