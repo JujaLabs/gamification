cd70666478fdfe8d2bea31dc74b561dfc4184d79
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-03-13 17:41:08 +0200

    #42 Changed logback configuration file. Changed logging messages

--- src/main/java/juja/microservices/gamification/service/AchievementService.java
+++ src/main/java/juja/microservices/gamification/service/AchievementService.java
@@ -39,12 +39,12 @@ public class AchievementService {
         List<Achievement> userFromIdList = achievementRepository.getAllAchievementsByUserFromIdCurrentDateType(userFromId, AchievementType.DAILY);
 
         if (userFromIdList.size() == 0) {
-            logger.debug("Entered to 'if' block in addDaily method");
+            logger.debug("No one 'Daily' achievement for user '{}' at current date", userFromId);
             Achievement newAchievement = new Achievement(userFromId, userFromId, 1, description, AchievementType.DAILY);
-            logger.info("Added new daily achievement from user '{}'", userFromId);
+            logger.info("Added new 'Daily' achievement {}, from user '{}'", newAchievement.getId(), userFromId);
             return achievementRepository.addAchievement(newAchievement);
         } else {
-            logger.debug("Entered to 'else' block in addDaily method");
+            logger.debug("There is already one 'Daily' achievement for user '{}' at current date", userFromId);
             Achievement achievement = userFromIdList.get(0);
             logger.debug("Received achivement {}", achievement.getId());
             String oldDescription = achievement.getDescription();
@@ -56,7 +56,6 @@ public class AchievementService {
             achievement.setDescription(description);
             logger.debug("Description is set to achievement {}", achievement.getId());
             logger.info("Added description to daily achivement from user '{}'", userFromId);
-            logger.debug("Left the method addDaily");
             return achievementRepository.addAchievement(achievement);
         }
     }
@@ -70,13 +69,13 @@ public class AchievementService {
                 .getAllAchievementsByUserFromIdCurrentDateType(userFromId, AchievementType.THANKS);
 
         if (userFromId.equalsIgnoreCase(userToId)) {
-            logger.warn("User '{}' trying to put thanks to yourself", userFromId);
+            logger.warn("User '{}' trying to put 'Thanks' achievement to yourself", userFromId);
             throw new UnsupportedAchievementException("You cannot thank yourself");
         }
 
         for (Achievement achievement : userFromAndToListToday) {
             if (achievement.getTo().equals(userToId)) {
-                logger.warn("User '{}' tried to give thanks more than one times to person '{}'", userFromId, userToId);
+                logger.warn("User '{}' tried to give 'Thanks' achievement more than one times to person '{}'", userFromId, userToId);
                 throw new UnsupportedAchievementException("You cannot give more than one thanks for day one person");
             }
         }
@@ -84,21 +83,21 @@ public class AchievementService {
         if (userFromAndToListToday.isEmpty()) {
             Achievement firstThanks = new Achievement(userFromId, userToId, 1, description, AchievementType.THANKS);
             result.add(achievementRepository.addAchievement(firstThanks));
-            logger.info("Added first thanks from user '{}' to user '{}'",userFromId, userToId);
+            logger.info("Added first 'Thanks' achievement from user '{}' to user '{}'",userFromId, userToId);
             return result;
 
         } else if (userFromAndToListToday.size() >= TWO_THANKS) {
-            logger.warn("User '{}' tried to give thanks more than two times per day", userFromId);
+            logger.warn("User '{}' tried to give 'Thanks' achievement more than two times per day", userFromId);
             throw new UnsupportedAchievementException("You cannot give more than two thanks for day");
 
         } else {
             Achievement secondThanks = new Achievement(userFromId, userToId, 1, description, AchievementType.THANKS);
             result.add(achievementRepository.addAchievement(secondThanks));
-            logger.info("Added second thanks from user '{}' to user '{}'", userFromId, userToId);
+            logger.info("Added second 'Thanks' achievement from user '{}' to user '{}'", userFromId, userToId);
             String descriptionTwoThanks = "Issued two thanks";
             Achievement thirdThanks = new Achievement(userFromId, userFromId, 1, descriptionTwoThanks, AchievementType.THANKS);
             result.add(achievementRepository.addAchievement(thirdThanks));
-            logger.info("Added thanks achievement to user '{}' for the distributed two thanks to other users", userFromId);
+            logger.info("Added 'Thanks' achievement to user '{}' for the distributed two thanks to other users", userFromId);
         }
         return result;
     }
@@ -116,7 +115,7 @@ public class AchievementService {
         List<Achievement> codenjoyUsersToday = achievementRepository.getAllCodenjoyAchievementsCurrentDate();
 
         if (!codenjoyUsersToday.isEmpty()) {
-            logger.warn("User '{}' tried to give codenjoy points twice a day", userFromId);
+            logger.warn("User '{}' tried to give 'Codenjoy' achievement points twice a day", userFromId);
             throw new UnsupportedAchievementException("You cannot give codenjoy points twice a day");
         }
         if ("".equals(userFromId)) {
@@ -128,15 +127,15 @@ public class AchievementService {
             throw new UnsupportedAchievementException("First or second place user cannot be empty");
         }
         if (firstUserToId.equalsIgnoreCase(secondUserToId)) {
-            logger.warn("Codenjoy request rejected: first and second places users is same");
+            logger.warn("Codenjoy request rejected: first and second place users is same");
             throw new UnsupportedAchievementException("First and second users must be different");
         }
         if (firstUserToId.equalsIgnoreCase(thirdUserToId)) {
-            logger.warn("Codenjoy request rejected: first and third places users is same");
+            logger.warn("Codenjoy request rejected: first and third place users is same");
             throw new UnsupportedAchievementException("First and third users must be different");
         }
         if (secondUserToId.equalsIgnoreCase(thirdUserToId)) {
-            logger.warn("Codenjoy request rejected: second and third places users is same");
+            logger.warn("Codenjoy request rejected: second and third place users is same");
             throw new UnsupportedAchievementException("Second and third users must be different");
         }
     }
@@ -150,15 +149,15 @@ public class AchievementService {
                 "Codenjoy second place", AchievementType.CODENJOY);
 
         result.add(achievementRepository.addAchievement(firstPlace));
-        logger.info("Added fist place Codenjoy achievement from user '{}' to '{}'", request.getFrom(), request.getFirstPlace());
+        logger.info("Added fist place 'Codenjoy' achievement from user '{}' to '{}'", request.getFrom(), request.getFirstPlace());
         result.add(achievementRepository.addAchievement(secondPlace));
-        logger.info("Added second place Codenjoy achievement from user '{}' to '{}'", request.getFrom(), request.getSecondPlace());
+        logger.info("Added second place 'Codenjoy' achievement from user '{}' to '{}'", request.getFrom(), request.getSecondPlace());
 
         if (!"".equals(request.getThirdPlace())) {
             Achievement thirdPlace = new Achievement(request.getFrom(), request.getThirdPlace(), CODENJOY_THIRD_PLACE,
                     "Codenjoy third place", AchievementType.CODENJOY);
             result.add(achievementRepository.addAchievement(thirdPlace));
-            logger.info("Added third place Codenjoy achievement from user '{}' to '{}'", request.getFrom(), request.getThirdPlace());
+            logger.info("Added third place 'Codenjoy' achievement from user '{}' to '{}'", request.getFrom(), request.getThirdPlace());
         }
         return result;
     }
@@ -172,7 +171,7 @@ public class AchievementService {
             throw new UnsupportedAchievementException("You must be enter interview report");
         } else {
             Achievement newAchievement = new Achievement(userFromId, userFromId, INTERVIEW_POINTS, description, AchievementType.INTERVIEW);
-            logger.info("Added interview achievement from user '{}'", userFromId);
+            logger.info("Added 'Interview' achievement from user '{}'", userFromId);
             return achievementRepository.addAchievement(newAchievement);
         }
     }
