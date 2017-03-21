c500b331b0ed4f771f3a5eae8a75638fbf5350bb
O.Kulikova <o.kulikova@avalonfundaktiv.com>
2017-03-21 16:54:17 +0200

    added cors filter

--- src/main/java/juja/microservices/gamification/Gamification.java
+++ src/main/java/juja/microservices/gamification/Gamification.java
@@ -2,11 +2,30 @@ package juja.microservices.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Gamification {
 
     public static void main(String[] args) {
         SpringApplication.run(Gamification.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://gamification.juja.com.ua");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
