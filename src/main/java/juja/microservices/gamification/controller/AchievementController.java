c790c9e3dc826a2966325c662673411ebdf13fb6
starikivan <Ubnkth99>
2017-04-04 15:11:08 +0300

    #78: Add Swagger 2 for API UI

--- src/main/java/juja/microservices/gamification/controller/AchievementController.java
+++ src/main/java/juja/microservices/gamification/controller/AchievementController.java
@@ -1,5 +1,8 @@
 package juja.microservices.gamification.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import juja.microservices.gamification.entity.CodenjoyRequest;
 import juja.microservices.gamification.entity.DailyRequest;
 import juja.microservices.gamification.entity.InterviewRequest;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.net.HttpURLConnection;
 import java.util.List;
 
 /**
@@ -26,6 +30,15 @@ public class AchievementController {
 
     @RequestMapping(value = "/achieve/daily", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(
+            value = "Add points for daily report",
+            notes = "This method adds points for daily report"
+    )
+    //TODO @ApiImplicitParams doesn`t works
+    //    @ApiImplicitParams(value - @ApiImplicitParam(name, value, dataType, required))
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns achievement id.")
+    })
     public ResponseEntity<?> addDaily(@RequestBody DailyRequest request) {
         String achievementId = achievementService.addDaily(request);
         logger.info("Added daily achievement, id = {}", achievementId);
@@ -34,6 +47,14 @@ public class AchievementController {
 
     @RequestMapping(value = "/achieve/thanks", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(
+            value = "Add points for thanks one user by another",
+            notes = "This method adds points for help one user by another"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "First thanks per day returns  achievement id.\n" +
+                    "Second thanks per day return array of achievement ids."),
+    })
     public ResponseEntity<?> addThanks(@RequestBody ThanksRequest request) {
         List<String> ids = achievementService.addThanks(request);
         logger.info("Added thanks achievement, ids = {}", ids.toString());
@@ -42,6 +63,13 @@ public class AchievementController {
 
     @RequestMapping(value = "/achieve/codenjoy", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(
+            value = "Add points for codenjoy winners",
+            notes = "This method adds points for winners in codenjoy tournament"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns array of achievement ids."),
+    })
     public ResponseEntity<?> addCodenjoy(@RequestBody CodenjoyRequest request) {
         List<String> ids = achievementService.addCodenjoy(request);
         logger.info("Added codenjoy achievement, ids = {}", ids.toString());
@@ -50,6 +78,13 @@ public class AchievementController {
 
     @RequestMapping(value = "/achieve/interview", method = RequestMethod.POST)
     @ResponseBody
+    @ApiOperation(
+            value = "Add points for interview",
+            notes = "This method adds points for successful or unsuccessful interview"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns  achievement id."),
+    })
     public ResponseEntity<?> addInterview(@RequestBody InterviewRequest request) {
         String achievementId = achievementService.addInterview(request);
         logger.info("Added daily achievement, id = {}", achievementId);
--- src/main/java/juja/microservices/gamification/controller/UserController.java
+++ src/main/java/juja/microservices/gamification/controller/UserController.java
@@ -1,5 +1,8 @@
 package juja.microservices.gamification.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import juja.microservices.gamification.entity.UserIdsRequest;
 import juja.microservices.gamification.entity.UserAchievementDetails;
 import juja.microservices.gamification.entity.UserPointsSum;
@@ -11,16 +14,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.net.HttpURLConnection;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/user",consumes = "application/json", produces = "application/json")
+@RequestMapping(value = "/user", consumes = "application/json", produces = "application/json")
 public class UserController {
 
     @Inject
     private UserService userService;
 
     @RequestMapping(value = "/pointSum", method = RequestMethod.GET)
+    @ApiOperation(
+            value = "Get total points for all users",
+            notes = "This method returns total points for all users"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns total points for all users"),
+    })
     public ResponseEntity<?> getAllUsersWithPointSum() {
         List<UserPointsSum> users = userService.getAllUsersWithPointSum();
 
@@ -28,6 +39,13 @@ public class UserController {
     }
 
     @RequestMapping(value = "/achieveDetails", method = RequestMethod.POST)
+    @ApiOperation(
+            value = "Get achievement details for some users",
+            notes = "This method returns detailed information for selected users"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Returns full information for selected users"),
+    })
     public ResponseEntity<?> getUsersWithAchievementDetails(@RequestBody UserIdsRequest toIds) {
         List<UserAchievementDetails> result =
                 userService.getUserAchievementsDetails(toIds);
