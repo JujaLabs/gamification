e7509bb75c12a3f16c2e89c8b122d50c17ecca8f
Ben Novikov <bigopendatanet@gmail.com>
2017-11-27 15:47:37 +0200

    #128 KeeperClient added RestKeeperRepo modified
    Feign dependencies and annotations added to build.gradle, app.properties, Gamification

--- src/main/java/juja/microservices/gamification/dao/impl/RestKeeperRepository.java
+++ src/main/java/juja/microservices/gamification/dao/impl/RestKeeperRepository.java
@@ -1,44 +1,40 @@
 package juja.microservices.gamification.dao.impl;
 
+import feign.FeignException;
+import juja.microservices.gamification.dao.KeeperClient;
 import juja.microservices.gamification.dao.KeeperRepository;
 import juja.microservices.gamification.entity.KeeperDTO;
 import juja.microservices.gamification.exceptions.KeepersMicroserviceExchangeException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import javax.inject.Inject;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author Vadim Dyachenko
+ * @author Benjamin Novikov
  */
 @Repository
+@Slf4j
 public class RestKeeperRepository implements KeeperRepository {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private KeeperClient keeperClient;
 
-    @Inject
-    private RestTemplate restTemplate;
-
-    @Value("${keepers.endpoint.getKeepers}")
-    private String keepersGetKeepersUrl;
+    public RestKeeperRepository(KeeperClient keeperClient) {
+        this.keeperClient = keeperClient;
+    }
 
     @Override
     public List<KeeperDTO> getKeepers() {
+        log.debug("Keepers service request has been sent");
         List<KeeperDTO> result;
-        logger.debug("Send request to keeper repository");
         try {
-            ResponseEntity<KeeperDTO[]> response = this.restTemplate.getForEntity(keepersGetKeepersUrl, KeeperDTO[].class);
-            result = Arrays.asList(response.getBody());
-        } catch (HttpClientErrorException ex) {
+            result = keeperClient.getKeepers();
+            log.debug("Request to Keepers service has finished");
+        } catch (FeignException ex) {
             throw new KeepersMicroserviceExchangeException("Keepers microservice Exchange Error: " + ex.getMessage());
         }
-        logger.debug("Received list of active keeper: {}", result);
+        log.info("Received list of active keepers: {}", result);
+
         return result;
     }
 }
