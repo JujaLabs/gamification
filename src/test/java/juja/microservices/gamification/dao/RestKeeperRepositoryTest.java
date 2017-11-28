2ae7d1892c96a8772230ed4fd024e89023a1de8f
Ben Novikov <bigopendatanet@gmail.com>
2017-11-29 00:38:08 +0200

    Revert "RestKeeperRepositoryTest"
    This reverts commit 3b086089fa84e2507a0d73d0588599fcd1f14ba1.

--- src/test/java/juja/microservices/gamification/dao/RestKeeperRepositoryTest.java
+++ src/test/java/juja/microservices/gamification/dao/RestKeeperRepositoryTest.java
@@ -1,81 +1,83 @@
 package juja.microservices.gamification.dao;
 
-import feign.FeignException;
 import juja.microservices.gamification.entity.KeeperDTO;
 import juja.microservices.gamification.exceptions.KeepersMicroserviceExchangeException;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-/**
- * @author Ivan Shapovalov
- * @author Benjamin Novikov
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestKeeperRepositoryTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
     @Inject
     private KeeperRepository keeperRepository;
-
-    @MockBean
-    private KeeperClient keeperClient;
+    @Inject
+    private RestTemplate restTemplate;
+    private MockRestServiceServer mockServer;
 
     @Value("${keepers.endpoint.getKeepers}")
     private String keepersGetKeepersUrl;
 
-    @Test
-    public void getKeepers_KeeperClientRequest_ReturnsListOfTwoKeeperDTOs() {
-        //given
-        List<KeeperDTO> expectedList = new ArrayList<>(Arrays.asList(
-                new KeeperDTO("0002A", Arrays.asList("First direction","Second direction")),
-                new KeeperDTO("0002B", Collections.singletonList("Third direction"))));
-        when(keeperClient.getKeepers()).thenReturn(expectedList);
-
-        //when
-        List<KeeperDTO> result = keeperRepository.getKeepers();
-
-        //then
-        assertEquals(result.size(), 2);
-        assertThat(result, equalTo(expectedList));
-        verify(keeperClient).getKeepers();
-        verifyNoMoreInteractions(keeperClient);
+    @Before
+    public void setup() {
+        mockServer = MockRestServiceServer.bindTo(restTemplate).build();
     }
 
-    @Test(expected = KeepersMicroserviceExchangeException.class)
-    public void getKeepers_ExceptionThrown_IsKeepersMicroserviceExchangeException() {
-        //given
-        FeignException feignException = mock(FeignException.class);
-        when(keeperClient.getKeepers()).thenThrow(feignException);
+    @Test
+    public void getKeepers() {
+        mockServer.expect(requestTo(keepersGetKeepersUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(
+                        "[{\"uuid\":\"0002A\",\"directions\":[\"First direction\",\"Second direction\"]}," +
+                                "{\"uuid\":\"0002B\",\"directions\":[\"Third direction\"]}]",
+                        MediaType.APPLICATION_JSON));
 
-        //when
-        try {
-            keeperRepository.getKeepers();
-        }
+        List<String> firstKeeperDirections = Arrays.asList("First direction", "Second direction");
+        List<String> secondKeeperDerections = Arrays.asList("Third direction");
 
-        //then
-        finally {
-            verify(keeperClient).getKeepers();
-            verifyNoMoreInteractions(keeperClient);
-        }
+        List<KeeperDTO> expectedList = new ArrayList<>();
+        expectedList.add(new KeeperDTO("0002A", firstKeeperDirections));
+        expectedList.add(new KeeperDTO("0002B", secondKeeperDerections));
+
+        List<KeeperDTO> result = keeperRepository.getKeepers();
+
+        assertThat(result, equalTo(expectedList));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenUserServiceThrowExceptinon() {
+        mockServer.expect(requestTo(keepersGetKeepersUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withBadRequest().body("bad request"));
+
+        thrown.expect(KeepersMicroserviceExchangeException.class);
+        thrown.expectMessage(containsString("Keepers microservice Exchange Error: "));
+
+        keeperRepository.getKeepers();
     }
 }
